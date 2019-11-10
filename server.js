605500e29bd5e976d377ea97888bb04e29295829
pmatrix <pmatrixmedia@gmail.com>
2019-11-10 14:14:58 -0600

    edit express

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
@@ -12,8 +13,5 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-router.route('/basicapp')
-   .get((req, res) => {
-      res.sendFile(process.cwd()+'/server/vr/index.html')
 
 app.listen(process.env.PORT || 8081);
